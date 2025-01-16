501d885a70f4390cc00eb55a1f9f9f92947e5411
Kshitija <kshitijachilbule5@gmail.com>
2025-01-16 19:29:12 +0530

    .gitignore file added

--- Diamond Price Prediction/setup.py
+++ Diamond Price Prediction/setup.py
@@ -10,6 +10,9 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n", "") for req in requirements]
+        
+        return requirements
+    
 
 setup(
     name='DiamondPricePrediction',
